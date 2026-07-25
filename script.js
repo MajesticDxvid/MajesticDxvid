34e135b336fec03c9e33b65b39b51c530afdb2e4
MajesticDxvid <altname42@gmail.com>
2026-07-26 00:32:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+function openVideo(videoId) {
+  const modal = document.getElementById("videoModal");
+  const frame = document.getElementById("videoFrame");
+
+  frame.src = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
+  modal.style.display = "flex";
+}
+
+function closeVideo() {
+  const modal = document.getElementById("videoModal");
+  const frame = document.getElementById("videoFrame");
+
+  frame.src = "";
+  modal.style.display = "none";
+}
+
+document.getElementById("year").textContent = new Date().getFullYear();
